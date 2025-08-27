d3cba54dc542ae8683b9b130556affc546ef5e47
Nathan Caudeli <nathan.caudeli@hotmail.com>
2025-08-27 18:13:30 +0200

    Revert "test"
    This reverts commit 3b29e95a2f060664654825aa4c60a7529405e6f7.

--- api/webhook.js
+++ api/webhook.js
@@ -7,8 +7,6 @@ const { generateAds } = require('../lib/generator');
 const { parseEmailContent } = require('../lib/parser');
 const { sendAdsGeneratedEmail } = require('../lib/email-sender');
 const checkGmailHandler = require('./check-gmail');
-const textSquareHandler = require('./text-square');
-const textStoryHandler = require('./text-story');
 
 const app = express();
 
@@ -163,10 +161,6 @@ app.get('/api/test', async (req, res) => {
 
 // Endpoint pour vérifier Gmail et générer des visuels
 app.all('/api/check-gmail', checkGmailHandler);
-
-// Endpoints pour configurer les textes
-app.all('/api/text-square', textSquareHandler);
-app.all('/api/text-story', textStoryHandler);
 
 // Endpoint de test email
 app.post('/api/test-email', async (req, res) => {
